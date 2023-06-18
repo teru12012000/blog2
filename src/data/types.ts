47c506dc3de694ad44c858d77f819910e9c6f6bb
teru12012000 <sion0012@outlook.jp>
2023-06-19 00:41:02 +0900

    firstCommit

---
+++ src/data/types.ts
@@ -0,0 +1,10 @@
+export type imgType={
+  url:string;
+  height:number;
+  width:number;
+}
+export type blogMenuType={
+  id:string;
+  title:string;
+  img:imgType;
+}
---
+++ src/lib/lib.ts
@@ -0,0 +1,10 @@
+import { MicroCMSQueries, createClient } from "microcms-js-sdk";
+
+const client=createClient({
+  serviceDomain:import.meta.env.DOMAIN as string,
+  apiKey:import.meta.env.API_KEY as string,
+})
+
+export const getBlogDetail=async(queries:MicroCMSQueries)=>{
+  return await client.get({endpoint:"blog2",queries:queries})
+}
